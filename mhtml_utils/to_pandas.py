3d5935e67f0916167b22c1b223c06a94bb7ad5c5
Viet Le <vietlq85@gmail.com>
2020-04-15 22:42:55 +0100

    generic parent tag

--- mhtml_utils/to_pandas.py
+++ mhtml_utils/to_pandas.py
@@ -19,7 +19,7 @@ import pandas as pd
 # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_excel.html
 # Make sure `openpyxl` is installed
 
-_STR_XPATH_LI_SPAN = '//li[span[contains(text(), " v ")]]'
+_STR_XPATH_PARENT_SPAN = '//{parent}[span[contains(text(), " v ")]]'
 _STR_CHILD_SPAN_WITH_V = "./span[contains(text(), ' v ')]"
 _STR_HTML_NS = "http://www.w3.org/TR/REC-html40"
 
@@ -34,16 +34,17 @@ def extract_spans_with_v(elem):
     return [extract_text(sub_elem) for sub_elem in elem.xpath(_STR_CHILD_SPAN_WITH_V)]
 
 
-def extract_li_and_spans_with_v(top_elem, redact_cases=True):
+def extract_parent_and_spans_with_v(top_elem, parent, redact_cases=True):
     results = [
-        (extract_spans_with_v(li), extract_text(li)) for li in top_elem.xpath(_STR_XPATH_LI_SPAN)
+        (extract_spans_with_v(parent_elem), extract_text(parent_elem))
+        for parent_elem in top_elem.xpath(_STR_XPATH_PARENT_SPAN.format(parent=parent))
     ]
     # Redact case names for challenge
     output = []
-    for (spans_with_v, li) in results:
+    for (spans_with_v, parent_elem) in results:
         for span in spans_with_v:
-            li = li.replace(span, "****_guess_it_****")
-        output.extend([(span, li) for span in spans_with_v])
+            parent_elem = parent_elem.replace(span, "****_guess_it_****")
+        output.extend([(span, parent_elem) for span in spans_with_v])
 
     return pd.DataFrame(data=output, columns=["case_name", "details"])
 
@@ -53,4 +54,9 @@ def dataframe_from_content(raw_content, redact_cases=True):
     # Export a valid and prettified HTML document
     html = soup.prettify()
     doc = parsel.Selector(text=html)
-    return extract_li_and_spans_with_v(doc, redact_cases=redact_cases)
+    return pd.concat(
+        [
+            extract_parent_and_spans_with_v(doc, parent=parent, redact_cases=redact_cases)
+            for parent in ["li", "p"]
+        ]
+    )
